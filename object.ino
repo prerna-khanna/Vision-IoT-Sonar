8b588e335b3612b283106594493dbfe4cb9f3c09
Prerna Khanna <35433654+prerna-khanna@users.noreply.github.com>
2018-12-06 16:08:04 +0530

    Add files via upload

---
+++ object.ino
@@ -0,0 +1,21 @@
+String val;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {  
+  Serial.begin(9600);  
+  lcd.begin(16, 2);
+ 
+}
+
+void loop() {
+  lcd.clear();
+  val = Serial.readString(); 
+  delay(1000);  
+  Serial.println(val);
+  delay(10);
+
+  lcd.print(val);
+  lcd.display();
+ // delay(5000000);
+}
